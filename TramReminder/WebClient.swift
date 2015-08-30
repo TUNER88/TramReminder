4bdcadae0d3076b9b7e2d3fc06c3feef1a35c2da
TUNER88 <anton.pauli@gmail.com>
2015-08-31 00:54:43 +0200

    add statusBar icon

--- TramReminder/WebClient.swift
+++ TramReminder/WebClient.swift
@@ -1,8 +1,8 @@
 //
-//  ViewController.swift
+//  WebClient.swift
 //  TramReminder
 //
-//  Created by Anton Pauli on 27.08.15.
+//  Created by Anton Pauli on 30.08.15.
 //  Copyright (c) 2015 Anton Pauli. All rights reserved.
 //
 
@@ -10,66 +10,68 @@ import Cocoa
 import WebKit
 import SwiftDate
 
-class ViewController: NSViewController {
-
-    let origin: String = "Jena, Stadtzentrum, Löbdergraben"
-    let destination: String = "Jena, Lobeda-West"
+class WebClient: NSObject {
+    
+    var webView: WebView = WebView()
+    
+    var origin: String
+    var destination: String
     
     var requestCounter: Int = 0
-    var rides: NSMutableArray = []
     
     var ridesLoaded: ((NSMutableArray) -> (Void))?
     var ridesLoadingFailed: ((Void) -> (Void))?
     
-    @IBOutlet weak var webView: WebView!
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    init(origin: String, destination: String) {
+        self.origin = origin;
+        self.destination = destination;
         
-//        self.webView.frameLoadDelegate = self
-//        
-//                    self.loadRides({ (data) -> Void in
-//                        println("ok")
-//                        }, failure: {
-//                            println("NOT OK")
-//                    })
-        
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+        super.init()
+        self.webView.frameLoadDelegate = self
     }
     
-    override func webView(sender: WebView!, didFinishLoadForFrame frame: WebFrame!) {
+
+    
+    override func webView(sender: WebView, didFinishLoadForFrame frame: WebFrame) {
+        
         if(self.requestCounter == 1) {
             self.fillForm()
             self.submitForm()
-        } else if (self.requestCounter == 2) {
+        } else if(self.requestCounter == 2) {
+            self.loadLaterItems()
+        } else if(self.requestCounter == 3) {
+            self.loadDetails()
+        } else if (self.requestCounter == 4) {
             let rideCount = self.getRideCount()
+            var rides = NSMutableArray()
+            
+            if(rideCount == 0){
+                println("No rides found")
+                ridesLoadingFailed!()
+                return
+            }
             
             for index in 0...rideCount-1 {
                 
                 let arrival = self.getArrival(index);
                 let depature = self.getDeparture(index);
+                let origin = self.getOrigin(index);
+                let destination = self.getDestination(index);
                 
                 let ride = Ride(
-                    origin: self.origin,
-                    destination: self.destination,
+                    origin: origin,
+                    destination: destination,
                     departure: depature,
                     arrival: arrival,
                     changes: -1
                 )
                 
-                self.rides.addObject(ride);
+                rides.addObject(ride);
             }
             
-            self.ridesLoaded!(self.rides)
+            self.ridesLoaded!(rides)
         }
-    
+        
         self.requestCounter++
     }
     
@@ -80,6 +82,16 @@ class ViewController: NSViewController {
     
     func submitForm() {
         let command = "document.querySelector('input[accesskey=s]').click()"
+        self.executeJsCommand(command)
+    }
+    
+    func loadLaterItems() {
+        let command = "document.querySelector('a[accesskey=l]').click()"
+        self.executeJsCommand(command)
+    }
+    
+    func loadDetails() {
+        let command = "document.querySelector('input[accesskey=a]').click()"
         self.executeJsCommand(command)
     }
     
@@ -105,10 +117,10 @@ class ViewController: NSViewController {
     }
     
     func getDeparture(rideIndex: Int) -> NSDate {
-        let timeCommand = "document.querySelectorAll('[headers=hafasOVTime]')[\(rideIndex)].innerText.split('an')[0].substring(3).trim()"
+        let timeCommand = "document.querySelector('[headers=hafasDTL\(rideIndex)_TimeDep]').innerText.trim()"
         let timeString = self.executeJsCommand(timeCommand)
         
-        let dateCommand = "document.querySelectorAll('[headers=hafasOVDate]')[\(rideIndex)].innerText.trim()"
+        let dateCommand = "document.querySelectorAll('[headers=hafasOVDate]')[\(rideIndex)].innerText.trim().substring(null,8)"
         let dateString = self.executeJsCommand(dateCommand)
         
         let dateTimeString = "\(timeString) \(dateString)"
@@ -116,18 +128,30 @@ class ViewController: NSViewController {
     }
     
     func getArrival(rideIndex: Int) -> NSDate {
-        let timeCommand = "document.querySelectorAll('[headers=hafasOVTime]')[\(rideIndex)].innerText.split('an')[1].trim()"
+        let timeCommand = "document.querySelectorAll('[headers=hafasDTL\(rideIndex)_TimeArr]')[document.querySelectorAll('[headers=hafasDTL\(rideIndex)_TimeArr]').length-1].innerText.trim()"
         let timeString = self.executeJsCommand(timeCommand)
         
-        let dateCommand = "document.querySelectorAll('[headers=hafasOVDate]')[\(rideIndex)].innerText.trim()"
+        let dateCommand = "document.querySelectorAll('[headers=hafasOVDate]')[\(rideIndex)].innerText.trim().slice(-8)"
         let dateString = self.executeJsCommand(dateCommand)
         
         let dateTimeString = "\(timeString) \(dateString)"
+        
         return NSDate.date(fromString: dateTimeString, format: DateFormat.Custom("HH:mm dd.MM.yy"))!
+    }
+    
+    func getOrigin(rideIndex: Int) -> String {
+        let command = "document.querySelectorAll('[headers=hafasDTL\(rideIndex)_Stop]')[0].innerText"
+        return self.executeJsCommand(command)
+    }
+    
+    func getDestination(rideIndex: Int) -> String {
+        let command = "document.querySelectorAll('[headers=hafasDTL\(rideIndex)_Stop]')[document.querySelectorAll('[headers=hafasDTL\(rideIndex)_Stop]').length-1].innerText"
+        return self.executeJsCommand(command)
     }
     
     func loadRides(success : (NSMutableArray) -> Void, failure : Void -> Void)
     {
+        self.requestCounter = 0;
         let urlString = "http://vmt.hafas.de"
         
         self.webView.mainFrame.loadRequest(NSURLRequest(URL: NSURL(string: urlString)!))
@@ -137,10 +161,4 @@ class ViewController: NSViewController {
         self.ridesLoadingFailed = failure;
         
     }
-    
-    func loadRidesSimple(success : (NSMutableArray) -> Void)
-    {
-        success(self.rides);
-    }
 }
-
