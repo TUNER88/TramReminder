ef1b7721069dd9bf4e8502561c2ff8b225a07f1e
TUNER88 <anton.pauli@gmail.com>
2015-08-31 01:00:17 +0200

    add statusBar item and detail popover

--- TramReminder/AppDelegate.swift
+++ TramReminder/AppDelegate.swift
@@ -9,18 +9,107 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
+class AppDelegate: NSObject, NSApplicationDelegate, PreferencesWindowDelegate {
+    
+    let menu = NSMenu()
+    let popover = NSPopover()
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    var rides = NSMutableArray()
+    let detailViewController = DetailsViewController(nibName: "DetailsViewController", bundle: nil)
+    
+    var viewRefreshTimer = NSTimer()
+    var dataRefreshTimer = NSTimer()
+    
+    var viewRefreshInterval = 60.0
+    var dataRefreshInterval = 300.0
+    
+    // Jena, Stadtzentrum, Löbdergraben
+    // Jena, Lobeda-West
+    var vc = WebClient(origin: "", destination: "")
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        
+        loadConfigs()
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarIcon")
+            button.action = Selector("togglePopover:")
+        }
+        
+        statusItem.title = "Loading..."
+        popover.contentViewController = self.detailViewController
+        
+        self.viewRefreshTimer = NSTimer.scheduledTimerWithTimeInterval(viewRefreshInterval, target: self, selector: Selector("refreshViews"), userInfo: nil, repeats: true)
+        self.dataRefreshTimer = NSTimer.scheduledTimerWithTimeInterval(dataRefreshInterval, target: self, selector: Selector("reloadRides"), userInfo: nil, repeats: true)
+
+        
+        self.reloadRides()
+
     }
+    
+    func preferencesDidUpdate() {
+        loadConfigs()
+        reloadRides()
+    }
+    
+    func loadConfigs(){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        vc.origin = defaults.stringForKey("origin") ?? ""
+        vc.destination = defaults.stringForKey("destination") ?? ""
+    }
+    
+    func reloadRides(){
+        self.vc.loadRides({ (data) -> Void in
+            self.rides = data;
+            self.refreshViews()
+            
+            println("ok")
+        }, failure: {
+                println("NOT OK")
+        })
+    }
+    
+    func refreshViews(){
+        self.statusItem.title = self.getStatusbarText(self.rides)
+        self.detailViewController!.rides = self.rides
+    }
+    
+    func getStatusbarText(rides: NSMutableArray) -> String {
+        var text = ""
+        let separator = " / "
+        
+        for (index, object) in enumerate(rides) {
+            if let ride = object as? Ride {
+                text += ride.timeUntilDeparture() + (index == rides.count-1 ? "" : separator)
+            }
+        }
+        
+        return text
+    }
+    
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSMinYEdge)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    } 
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    
 
 }
 
---
+++ TramReminder/DetailsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailsViewController.swift
+//  TramReminder
+//
+//  Created by Anton Pauli on 30.08.15.
+//  Copyright (c) 2015 Anton Pauli. All rights reserved.
+//
+
+import Cocoa
+import SwiftDate
+
+class DetailsViewController: NSViewController {
+    
+    var preferencesWindow: PreferencesWindow!
+    @IBOutlet weak var rideDescriptions: NSTextField!
+    var rides: NSMutableArray = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferencesWindow = PreferencesWindow()
+        rideDescriptions.stringValue = "Loading..."
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        self.displayRides()
+    }
+    
+    func displayRides() {
+        var text = ""
+        
+        for (index, object) in enumerate(self.rides) {
+            if let ride = object as? Ride {
+                text += ride.departure.toShortTimeString() + " - " + ride.origin + "\n"
+                text += ride.arrival.toShortTimeString() + " - " + ride.destination + "\n"
+                text += "Duration: \(ride.durationToString()) \n"
+                text += "\n"
+            }
+        }
+        
+        rideDescriptions.stringValue = text
+    }
+    @IBAction func preferencesClicked(sender: NSButtonCell) {
+        preferencesWindow.showWindow(nil)
+    }
+}
